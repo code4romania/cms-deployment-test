09ac25543a39cfba7aad646ef871323389292de1
Andrei Ioniță <hi@andrei.io>
2020-11-28 21:41:58 +0000

    wip

--- resources/views/front/donate.blade.php
+++ resources/views/front/donate.blade.php
@@ -1,40 +1,55 @@
 {{-- This file isn't used anywhere. It only exists for purgecss --}}
 
 <div class="py-12 bg-success-300">
-    <div class="container max-w-4xl px-6 py-8 bg-white">
-        <div class="mb-8 leading-tight">
-            <h1 class="mb-1 text-3xl font-bold md:text-4xl lg:text-5xl">Digitalizăm România împreună</h1>
-            <h2 class="text-lg font-bold md:text-xl text-success-500">În ultimul an, milioane de români au folosit aplicații dezvoltate pro bono de Code for Romania</h2>
-        </div>
-        <div class="mb-8 leading-relaxed">În fiecare zi, sute dintre prietenii și colegii tăi lucrează pentru a construi instrumente digitale utile pentru a ne face viața mai ușoară și pentru a face România mai bună. Ajută-ne să ajutăm și mai mult.</div>
-        <div class="mb-8">
-            <div class="mb-3">Fiecare donație recurentă ne ajută să construim soluții din ce în ce mai repede.</div>
-            <form class="grid gap-6 md:grid-cols-12" action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-                <div class="md:col-span-7">
-                    <input type="hidden" name="cmd" value="_s-xclick">
-                    <input type="hidden" name="hosted_button_id" value="YED2GFGKUFXBJ">
-                    <input type="hidden" name="on0">
-                    <input type="hidden" name="currency_code" value="EUR">
-                    <select id="donate-select" name="os0" class="w-full form-select">
-                        <option value="5">€5.00 / lună</option>
-                        <option value="10">€10.00 / lună</option>
-                        <option value="20">€20.00 / lună</option>
-                        <option value="50">€50.00 / lună</option>
-                        <option value="100">€100.00 / lună</option>
-                    </select>
-                </div>
-                <div class="md:col-span-5">
-                    <input type="submit" name="submit" class="relative block w-full px-6 py-2 font-semibold leading-snug tracking-wide text-center text-white shadow-md hover:shadow-lg focus:outline-none focus:shadow-md bg-success-500 focus:bg-success-600" value="Donează">
-                </div>
-            </form>
-        </div>
-        <div class="mb-8">
-            <div class="mb-3">Sau ne poți susține o singură dată. Fiecare donație ne ajuta.</div>
-            <div class="my-3">
-                <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&amp;hosted_button_id=C39VZYTTZGF82" class="relative block w-full px-6 py-2 font-semibold leading-snug tracking-wide text-center text-white shadow-md hover:shadow-lg focus:outline-none focus:shadow-md bg-success-500 focus:bg-success-600" target="_blank">Donează doar luna asta</a>
+    <div class="container grid gap-8 lg:grid-cols-7">
+        <div class="px-6 py-8 bg-white lg:col-span-5">
+            <div class="mb-8 leading-tight">
+                <h1 class="mb-1 text-3xl font-bold md:text-4xl lg:text-5xl">Digitalizăm România împreună</h1>
+                <h2 class="text-lg font-bold md:text-xl text-success-500">În ultimul an, milioane de români au folosit aplicații dezvoltate pro bono de Code for Romania</h2>
             </div>
-            <div class="my-3 text-base text-gray-900">
-                Tranzacția ta este in siguranța, vei fi redirecționat către PayPal pentru a finaliza procesul de donație. Îți mulțumim!
+            <div class="mb-8 leading-relaxed">În fiecare zi, peste 1800 de voluntari dau din timpul și talentul lor pentru a construi instrumente digitale utile pentru a ne face viața mai ușoară și pentru a face România mai bună. Cu ajutorul tău, <strong class="font-bold">putem</strong> face mai mult.</div>
+            <div class="mb-8">
+                <div class="mb-3">Fiecare donație recurentă ne ajută să construim soluții din ce în ce mai repede.</div>
+                <form class="grid gap-6 md:grid-cols-12" action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+                    <div class="md:col-span-7">
+                        <input type="hidden" name="cmd" value="_s-xclick">
+                        <input type="hidden" name="hosted_button_id" value="YED2GFGKUFXBJ">
+                        <input type="hidden" name="on0">
+                        <input type="hidden" name="currency_code" value="EUR">
+                        <select id="donate-select" name="os0" class="w-full form-select">
+                            <option value="5">€5.00 / lună</option>
+                            <option value="10">€10.00 / lună</option>
+                            <option value="20">€20.00 / lună</option>
+                            <option value="50">€50.00 / lună</option>
+                            <option value="100">€100.00 / lună</option>
+                        </select>
+                    </div>
+                    <div class="md:col-span-5">
+                        <input type="submit" name="submit" class="relative block w-full px-6 py-2 font-semibold leading-snug tracking-wide text-center text-white shadow-md hover:shadow-lg focus:outline-none focus:shadow-md bg-success-500 focus:bg-success-600" value="Donează">
+                    </div>
+                </form>
+            </div>
+            <div class="mb-8">
+                <div class="mb-3">Sau ne poți susține o singură dată. Fiecare donație ne ajuta.</div>
+                <div class="my-3">
+                    <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&amp;hosted_button_id=C39VZYTTZGF82" class="relative block w-full px-6 py-2 font-semibold leading-snug tracking-wide text-center text-white shadow-md hover:shadow-lg focus:outline-none focus:shadow-md bg-success-500 focus:bg-success-600" target="_blank">Donează doar luna asta</a>
+                </div>
+                <div class="my-3 text-base text-gray-900">
+                    Tranzacția ta este in siguranța, vei fi redirecționat către PayPal pentru a finaliza procesul de donație. Îți mulțumim!
+                </div>
+            </div>
+        </div>
+        <div class="flex flex-col px-6 py-8 bg-white lg:col-span-2">
+            <div class="flex flex-col justify-center flex-1">
+                <p>Trimite <strong class="font-bold text-success-500">PUTEM</strong> prin SMS la</p>
+                <a href="sms:8864" class="block text-6xl font-bold text-center text-success-500">8864</a>
+                <p>și donează lunar 4 euro pentru Code for Romania*</p>
+            </div>
+
+            <hr class="my-8">
+
+            <div class="text-xs italic">
+                * Valoarea Donaţiei este de 4 EUR/lună. Suma alocată cauzei este de 4 Euro. Nu se percepe TVA pentru donaţiile de pe abonament. În reţelele Digi Mobil, Orange şi Telekom România Mobile, pentru cartelele preplătite, TVA-ul a fost reţinut la achiziţionarea creditului. Pentru donaţiile de pe cartele preplătite, în reţeaua Vodafone utilizatorii nu plătesc TVA.Campanie realizată cu sprijinul Digi Mobil, Orange Romania, Telekom Romania Mobile şi Vodafone Romania.
             </div>
         </div>
     </div>
